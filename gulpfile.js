855683701f6192ebb8945a8b2431a95903fe27c9
Kirill <example@gmail.com>
2022-05-13 23:07:57 +0300

    upgrade subheader

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -14,6 +17,14 @@ gulp.task('server', function() {
 gulp.task('styles', function() { 
     return gulp.src("src/sass/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({
+                prefix: "",
+                suffix: ".min",
+              }))
+            .pipe(autoprefixer({
+                cascade: false
+            }))
+            .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
 })
@@ -22,5 +33,5 @@ gulp.task('watch', function(){
     gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel('styles'))
     gulp.watch("src/*.html").on("change", browserSync.reload);
 })
-
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+ 
